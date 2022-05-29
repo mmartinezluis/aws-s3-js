ab7466de053c29a2807ee85a88158d676b5f4687
Luis Martinez <luis_mmartinez@live.com>
2022-05-29 15:07:33 -0400

    add isoDate calculation and bucket policy to uploadFile method

--- src/index.js
+++ src/index.js
@@ -7,7 +7,34 @@ class S3Client {
     }
 
     uploadFile(file, key, dirName) {
-        
+
+        const tenMinutes = 6e5;
+        const isoDate = new Date(new Date().getTime() + tenMinutes).toISOString();
+        const date = isoDate.split("T")[0].split("-").join("");  // yyyymmdd
+        const formattedIso = isoDate.split("-").join("").split(":").join("").split(".").join("");
+
+        const policy = Buffer.from(
+                JSON.stringify(
+                    { 
+                        expiration: isoDate,
+                        conditions: [
+                            {"bucket": this.config.bucketName},
+                            {"acl": "public-read"},
+                            ["starts-with", "$key", ""],
+                            ["starts-with", "$Content-Type", ""],
+                            {"x-amz-meta-uuid": "14365123651274"},
+                            {"x-amz-server-side-encryption": "AES256"},
+                            ["starts-with", "$x-amz-meta-tag", ""],
+                            {"x-amz-credential": `${this.config.accessKeyId}/${date}/${this.config.region}/s3/aws4_request`},
+                            {"x-amz-algorithm": "AWS4-HMAC-SHA256"},
+                            {"x-amz-date": formattedIso }
+                        ] 
+                    }
+                )
+            )
+            .toString('base64')
+            .replaceAll(/[$\n\r]/g, "")
+
     }
 
     _request(uri, method, payload, callback) {
