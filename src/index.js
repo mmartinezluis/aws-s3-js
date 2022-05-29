36fbf37d87c3bf2319957ef2cdf503fb5cbf4659
Luis Martinez <luis_mmartinez@live.com>
2022-05-29 14:50:12 -0400

    create XMLHttpRequest client within the '_request' private method

--- src/index.js
+++ src/index.js
@@ -5,6 +5,21 @@ class S3Client {
     constructor(config) {
         this.config = config;
     }
+
+    _request(uri, method, payload, callback) {
+        let xhr = new XMLHttpRequest();
+        xhr.open(method, uri, true);
+        xhr.onreadystatechange = function() {
+            if(xhr.readyState === XMLHttpRequest.DONE) {
+                let statusCode = xhr.status;
+                if(callback){
+                    callback(statusCode, xhr);
+                }
+            }
+        }
+        xhr.send(payload);
+    }
+
 }
 
 module.exports = S3Client;
