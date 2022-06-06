b919f6d135c3b6a0d53a7b32eb290ef475cf1b6d
Luis Martinez <luis_mmartinez@live.com>
2022-06-06 13:35:20 -0400

    remove browserify dependency

--- src/index.mjs
+++ src/index.mjs
@@ -1,6 +1,6 @@
 import CryptoES from 'crypto-es';
 import { nanoid } from 'nanoid';
-import customBuffer from "buffer";
+import customBuffer from "..buffer.js";
 
 // const Buffer = require('buffer/').Buffer;
 // Object.defineProperty(exports, "__esModule", { value: true });
