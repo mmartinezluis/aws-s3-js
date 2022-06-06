1841f392a1c9a290104e185174ce8b58cc8f5607
Luis Martinez <luis_mmartinez@live.com>
2022-06-06 12:47:34 -0400

    add .js extension from import in test2.mjs

--- src/test2.mjs
+++ src/test2.mjs
@@ -1,5 +1,5 @@
 import CryptoES from 'crypto-es';
-import something from "./test"
+import something from "./test.js"
 let c= CryptoES;
 const dateKey = c.HmacSHA256("secret", "AWS4");
 export {dateKey, something } ;
