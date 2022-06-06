65b829be3a5f2dc0a917a168d99cd3e9353b0826
Luis Martinez <luis_mmartinez@live.com>
2022-06-06 11:02:43 -0400

    remove 'buffer' dependency

--- src/test.js
+++ src/test.js
@@ -1,14 +1,7 @@
-import customBuffer from 'buffer'
-// import toBuffer from "typedarray-to-buffer";
-
+import customBuffer from 'buffer';
 
 function foo(){
     return customBuffer.from("Hello")
-}
+};
 
-// function run(){
-//     const encoder = new TextEncoder();
-//     return toBuffer(encoder.encode("Hello"));
-// }
-
-export default foo
+export default foo;
