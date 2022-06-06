2fbd98e402b416fa7a6bb3a87d2f47282b593a11
Luis Martinez <luis_mmartinez@live.com>
2022-06-06 12:33:02 -0400

    fix undefined variable in test2.mjs

--- src/test2.mjs
+++ src/test2.mjs
@@ -1,5 +1,5 @@
 import CryptoES from 'crypto-es';
 // import something from "./test"
 let c= CryptoES;
-const dateKey = c.HmacSHA256(date, "AWS4");
+const dateKey = c.HmacSHA256("secret", "AWS4");
 export default dateKey;
