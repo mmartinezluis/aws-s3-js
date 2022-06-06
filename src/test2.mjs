631b90030bf7eb6962930c83c1702d99bb5ca9c6
Luis Martinez <luis_mmartinez@live.com>
2022-06-06 12:43:04 -0400

    add buffer to test2.mjs

--- src/test2.mjs
+++ src/test2.mjs
@@ -1,5 +1,5 @@
 import CryptoES from 'crypto-es';
-// import something from "./test"
+import something from "./test"
 let c= CryptoES;
 const dateKey = c.HmacSHA256("secret", "AWS4");
-export default dateKey;
+export {dateKey, something } ;
